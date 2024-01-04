c173f2f8639bd7fe84cb2f1eec1552ec1629804c
FullMooney <hs3235@naver.com>
2024-01-04 09:16:26 +0900

    init

---
+++ log/src/main/java/com/dev/log/LogConfigApplication.java
@@ -0,0 +1,13 @@
+package com.dev.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogConfigApplication.class, args);
+	}
+
+}
